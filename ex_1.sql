6e6c6ac7ab38e302618e2b30d34bfcb60c8747e6
Roberto Magno Mazzotta <150812681+robertomagno1@users.noreply.github.com>
2025-03-10 14:32:22 +0100

    Update ex_1.sql, foreign key

--- ex_1.sql
+++ ex_1.sql
@@ -5,9 +5,32 @@ create table Employee (
 	Name character(20) not null,
 	Surname character(20) not null,
 	Depart character(15),
-	Salary numeric(9) default 0,
+	Salary numeric(9) default 0, # if there's an exeption where there's no value for this... set automatically to zero ... 
 	City character(15),
 	foreign key(Depart) references
 		Department(DepName),
 	unique (Surname,Name)
 )
+
+# unique vs forein key : unique is on a pair of columns ... two peopole on the same app 
+
+
+ ##Predefined domains
+### – Character:
+char(n)or character(n)
+varchar(n) (or char varying(n))
+nchar(n)and nvarchar(n) (or nchar varying(n)) (UNICODE)
+## – Numeric:
+# int or  integer, smallint
+numeric, (or numeric(p), numeric(p,s))
+decimal, (or decimal(p), decimal(p,s))
+float, float(p), real , double precision
+##– Date, time:
+Date, time, timestamp
+time with timezone, timestamp with timezone
+## – Bit:
+bit(n)
+bit varying(n)
+## – Further domanis (introduced in SQL:1999)
+boolean
+BLOB, CLOB, NCLOB (binary/character large object)
