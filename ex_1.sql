f2cbb900dbf981c20d1bd7d556e42d1526551320
Roberto Magno Mazzotta <150812681+robertomagno1@users.noreply.github.com>
2025-03-10 14:28:13 +0100

    Update ex_1.sql

--- ex_1.sql
+++ ex_1.sql
@@ -1,13 +1,13 @@
 # example of basic concept of SQL :: 
 
 create table Employee (
-ID character(6) primary key,
-Name character(20) not null,
-Surname character(20) not null,
-Depart character(15),
-Salary numeric(9) default 0,
-City character(15),
-foreign key(Depart) references
-Department(DepName),
-unique (Surname,Name)
+	ID character(6) primary key,
+	Name character(20) not null,
+	Surname character(20) not null,
+	Depart character(15),
+	Salary numeric(9) default 0,
+	City character(15),
+	foreign key(Depart) references
+		Department(DepName),
+	unique (Surname,Name)
 )
